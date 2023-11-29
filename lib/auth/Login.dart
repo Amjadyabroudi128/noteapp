33f548e8e0c6d2e495e23c448271c9a9ec4a2f43
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-11-29 01:21:41 +0000

    login UI

--- lib/auth/Login.dart
+++ lib/auth/Login.dart
@@ -15,28 +15,104 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(20),
           child: ListView(
             children: [
               SingleChildScrollView(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(height: 35,),
-                    Container(
-                      alignment: Alignment.center,
-                      width: 90,
-                      height: 90,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade200,
-                        borderRadius: BorderRadius.circular(65),
-                      ),
-                      child: Image.asset("images/note.jpg",
-                        height: 80,
+                    Center(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 90,
+                        height: 90,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(65),
+                        ),
+                        child: Image.asset("images/note.jpg",
+                          height: 80,
+                        ),
                       ),
                     ),
+                    SizedBox(height: 15,),
+                    Text("Login", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 10,),
+                    Text("Login to continue using the app", style: TextStyle( color: Colors.grey),),
+                    SizedBox(height: 20,),
+                    Text("Email", style: TextStyle(fontWeight: FontWeight.bold),),
+                    SizedBox(height: 9,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter your email",
+                        hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
+                        contentPadding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+                        filled: true,
+                        fillColor: Colors.grey[200],
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide(color: Colors.grey)
+                        )
+                      ),
+                    ),
+                    SizedBox(height: 9,),
+                    Text("Password", style: TextStyle(fontWeight: FontWeight.bold),),
+                    SizedBox(height: 9,),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                          hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
+                          contentPadding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+                          filled: true,
+                          fillColor: Colors.grey[200],
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: BorderSide(color: Colors.grey),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: BorderSide(color: Colors.grey)
+                          )
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20, bottom: 20),
+                      alignment: Alignment.topRight,
+                        child: Text("forgot passowrd?"))
                   ],
-                ),
-              )
+                )
+              ),
+              MaterialButton(
+                height: 40,
+                  color: Colors.orange,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  onPressed: (){},
+                  child: Text("Login")),
+              SizedBox(height: 10,),
+              MaterialButton(
+                  height: 40,
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  onPressed: (){},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("login with google"),
+                      SizedBox(width: 10,),
+                      Image.asset("images/google.png", width: 30,),
+                    ],
+                  )
+              ),
             ],
           ),
         ),
