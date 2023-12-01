7f6a4e8927ff3b50ff46ba3074ee032a796f27a4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-01 00:39:41 +0000

    textfield component

---
+++ lib/components/textformfield.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextForm extends StatelessWidget {
+  final String hinttext;
+  final TextEditingController myController;
+  const CustomTextForm({ required this.hinttext, required this.myController});
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: myController,
+      decoration: InputDecoration(
+          hintText: hinttext,
+          hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
+          contentPadding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+          filled: true,
+          fillColor: Colors.grey[200],
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.grey)
+          )
+      ),
+    );
+  }
+}
