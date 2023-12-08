7dde9635f107aa659c8f27ac98f5d51f783f629e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-08 03:31:42 +0000

    google login

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -28,10 +29,18 @@ class _HomePageState extends State<HomePage> {
               Icons.logout,
             ),
             onPressed: () async {
+              GoogleSignIn googleSignIn = GoogleSignIn();
+              googleSignIn.disconnect();
               await FirebaseAuth.instance.signOut();
-              Navigator.of(context).pushReplacementNamed("login");
-
+              Navigator.of(context).pushNamedAndRemoveUntil("login", (route) => false);
             },
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Center(
+            child: Text("Welcome${FirebaseAuth.instance.currentUser!.email}"),
           )
         ],
       ),
