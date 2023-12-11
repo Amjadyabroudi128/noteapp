80d4bb04157d575b04fc1165b8a19294f562252a
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-11 00:22:20 +0000

    custom textfield

--- lib/components/textformfield.dart
+++ lib/components/textformfield.dart
@@ -4,13 +4,22 @@ import 'package:flutter/material.dart';
 class CustomTextForm extends StatelessWidget {
   final String hinttext;
   final TextEditingController myController;
-  const CustomTextForm({ required this.hinttext, required this.myController});
+  final bool? isObscureText;
+  final Widget? SuffixIcon;
+ const CustomTextForm
+  ({ required this.hinttext,
+  required this.myController,
+  required, this.isObscureText = false,
+  required this.SuffixIcon
+  });
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      obscureText: isObscureText!,
       controller: myController,
       decoration: InputDecoration(
           hintText: hinttext,
+          suffixIcon: SuffixIcon!,
           hintStyle: TextStyle(fontSize: 13, color: Colors.grey),
           contentPadding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
           filled: true,
