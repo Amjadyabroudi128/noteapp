dba6ad4a972478139816cc026bff928a7d9b4496
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-11 00:20:39 +0000

    custom button

--- lib/components/CostomButton.dart
+++ lib/components/CostomButton.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final VoidCallback onPressed;
   final String title;
-  const CustomButton({ super.key, required this.onPressed, required this.title});
+   CustomButton({ super.key, required this.onPressed, required this.title});
 
   @override
   Widget build(BuildContext context) {
