4b51dbd7bab62ee7e1bc8b1963b6c6c0c7302a41
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-13 02:11:51 +0000

    add data

---
+++ lib/Categories/add.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pushnotification/components/CostomButton.dart';
+import 'package:pushnotification/components/CustomTextFieldAdd.dart';
+
+class AddCategory extends StatefulWidget {
+  const AddCategory({Key? key}) : super(key: key);
+
+  @override
+  State<AddCategory> createState() => _AddCategoryState();
+}
+
+class _AddCategoryState extends State<AddCategory> {
+  GlobalKey<FormState> formState = GlobalKey<FormState>();
+  TextEditingController name = TextEditingController();
+  CollectionReference categories = FirebaseFirestore.instance.collection("categories");
+
+  AddCategory () async {
+
+    if (formState.currentState!.validate()){
+      try {
+        DocumentReference response = await categories.add(
+          {"name": name.text}
+        );
+        Navigator.of(context).pushReplacementNamed("homepage");
+      } catch(e) {
+        print("error ${e}");
+      }
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Category"),
+      ),
+      body: Form(
+        key: formState,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                child: CustomTextAdd(hinttext: "name", myController: name)),
+            CustomButton(onPressed: (){
+              AddCategory();
+            }, title: "add")
+          ],
+        ),
+      ),
+    );
+  }
+}
