4130bf2f71e2638f0a21f1e73fe9ad5426670aeb
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-17 00:49:58 +0000

    removed the toggle

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -18,7 +18,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List <QueryDocumentSnapshot> data =[];
   bool isLoading = true;
-  bool darkTheme = false;
   getData() async {
    QuerySnapshot querySnapshot
    = await FirebaseFirestore.instance.collection("categories")
@@ -63,14 +62,6 @@ class _HomePageState extends State<HomePage> {
               Navigator.of(context).pushNamedAndRemoveUntil("login", (route) => false);
             },
           ),
-          CupertinoSwitch(
-            value: darkTheme,
-            onChanged: (bool value){
-              setState(() {
-                darkTheme = value;
-              });
-            },
-          )
         ],
       ),
       body:isLoading ? Center(
