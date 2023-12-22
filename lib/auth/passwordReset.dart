4aff2a04ff310aa7f95e98c6bb1d500095bff8d1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2023-12-22 00:08:43 +0000

    layout changes

--- lib/auth/passwordReset.dart
+++ lib/auth/passwordReset.dart
@@ -24,10 +24,11 @@ class _PasswordResetState extends State<PasswordReset> {
           children: [
             SizedBox(height: 120,),
             Column(
+
                 children: [
                   Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("please give us your email to reset your password",
+                    padding: const EdgeInsets.all(25.0),
+                    child: Text("enter the email associated with your account to reset password",
                       style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
                   ),
                   SizedBox(height: 14,),
@@ -42,9 +43,30 @@ class _PasswordResetState extends State<PasswordReset> {
                     title: "Send",
                     onPressed: () async {
                         await FirebaseAuth.instance.sendPasswordResetEmail(email: emailController.text);
+                        ScaffoldMessenger.of(context).showSnackBar( SnackBar(
+                            content: Text("check your email")
+                        ));
                         Navigator.of(context).pushNamed("login");
 
                     },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("or", style: TextStyle(fontSize: 25),),
+                        InkWell(
+                          onTap: (){
+                            Navigator.of(context).pushNamed("login");
+                          },
+                          child: Container(
+                              margin: EdgeInsets.only(top: 20, bottom: 20),
+                              alignment: Alignment.topRight,
+                              child: Text(" login", style: TextStyle(color: Colors.red, fontSize: 25),)),
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
